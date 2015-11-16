76f14990f5ca37c2ada90bebbd3f27eeb5bf1764
caijf <caijf@corp.netease.com>
2015-11-16 17:51:04 +0800

    enum for datatype

---
+++ lib/nei/mobile/template/oc/enum.h
@@ -0,0 +1,10 @@
+//
+//  Enum.h
+//
+//  Created by {{conf.author}} on {{conf.month}}/{{conf.day}}/{{conf.year}}.
+//  Copyright © {{conf.year}} NetEase. All rights reserved.
+//
+//  Auto build by NEI Builder
+
+
+
