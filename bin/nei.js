e4f4b311527f66e704c5f32da826b5f4cfff11e3
HuntBao <gzooler@gmail.com>
2016-08-13 17:07:49 +0800

    nei3.0 update

--- bin/nei.js
+++ bin/nei.js
@@ -25,6 +25,7 @@ var options = {
         config = this.format(action, config);
         config.action = action;
         if (!config.key) {
+            this.log(`错误: 缺少项目的唯一标识 key, 请到 NEI 网站上的相应项目的"工具设置"中查看该 key 值`);
             this.show(action);
         } else {
             main.build(config);
