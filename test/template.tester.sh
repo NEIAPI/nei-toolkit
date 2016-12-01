a29f8315799134f712792543a7c57b7eaa7bf83c
zhenglu.zl <zhenglu.zl@alipay.com>
2016-12-01 21:35:56 +0800

    finish

--- test/template.tester.sh
+++ test/template.tester.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
+node ../bin/nei.js template -p ./templateTest/input -o ./templateTest/output -d ./templateTest/data.json -b ./templateTest/handlerbars.js -input test -w true
