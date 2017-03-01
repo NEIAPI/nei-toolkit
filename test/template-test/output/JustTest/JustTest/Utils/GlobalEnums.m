d95e70a9cd31a631a95b49337c2a445e5f80fb7d
AbnerZheng <abnerzheng@gmail.com>
2017-03-01 20:05:29 +0800

    发布准备，代码规范

---
+++ test/template-test/output/JustTest/JustTest/Utils/GlobalEnums.m
@@ -0,0 +1,13 @@
+//
+//  GlobalEnums.m
+//  JustTest
+//
+//  Created by Baitianyu on 05/12/2016.
+//  Copyright © 2016 Netease. All rights reserved.
+//
+
+#import "GlobalEnums.h"
+
+@implementation GlobalEnums
+
+@end
