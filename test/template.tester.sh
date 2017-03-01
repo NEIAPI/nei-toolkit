dbad1d17ece6b1062ff4830f0683a33c5ae4cb74
AbnerZheng <abnerzheng@gmail.com>
2017-03-01 20:10:17 +0800

    update rename:

--- test/template.tester.sh
+++ test/template.tester.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-node ../bin/nei.js template -p ./templateTest/input -o ./templateTest/output   -input test -w true -author abner -ProductName JustTest --logLevel error
+node ../bin/nei.js template -p ./template-test/input -o ./template-test/output   -input test -w true -author abner -ProductName JustTest --logLevel error
